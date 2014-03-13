38524f38dcb9879a68f7ec6da17fab311444de49
Lukas Spieß <lukas.spiess@gmail.com>
2014-03-14 00:27:29 +0100

    Subclassing UIView

---
+++ Hypnosister/BNRHypnosisView.m
@@ -0,0 +1,31 @@
+//
+//  BNRHypnosisView.m
+//  Hypnosister
+//
+//  Created by Lukas Spieß on 13.03.14.
+//  Copyright (c) 2014 Lukas Spieß. All rights reserved.
+//
+
+#import "BNRHypnosisView.h"
+
+@implementation BNRHypnosisView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
---
+++ Hypnosister/Hypnosister/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  Hypnosister
+//
+//  Created by Lukas Spieß on 13.03.14.
+//  Copyright (c) 2014 Lukas Spieß. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
