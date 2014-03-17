67e43e583dbd284710d0bf4c3a3965a610a2ebfa
Lukas Spieß <lukas.spiess@gmail.com>
2014-03-17 14:13:40 +0100

    ViewController initializers

--- HypnoNerd/HypnoNerd/BNRAppDelegate.m
+++ HypnoNerd/HypnoNerd/BNRAppDelegate.m
@@ -20,12 +20,7 @@
     // This line will generate a warning, ignore it for now
     BNRHypnosisViewController *hvc = [[BNRHypnosisViewController alloc] init];
     
-    
-    // This will get a pointer to an object that represents the app bundle
-    NSBundle *appBundle = [NSBundle mainBundle];
-    
-    // Look in the appBundle for the file BNRReminderViewController.xib
-    BNRReminderViewController *rvc = [[BNRReminderViewController alloc]initWithNibName:@"BNRReminderViewController" bundle:appBundle];
+    BNRReminderViewController *rvc = [[BNRReminderViewController alloc] init];
     
     UITabBarController *tbc = [[UITabBarController alloc]init];
     tbc.viewControllers = @[hvc,rvc];
